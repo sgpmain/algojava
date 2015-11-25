ed4c2932a1ddb55a539476e9bcacb8ee14ae4784
sgpmain <sgp.main@gmail.com>
2015-11-25 13:39:46 +0200

    arranged imports

--- src/test/java/my/algo/greedy/ScheduleJobsTest.java
+++ src/test/java/my/algo/greedy/ScheduleJobsTest.java
@@ -9,7 +9,6 @@ import org.junit.runners.Parameterized;
 
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.function.Supplier;
 
